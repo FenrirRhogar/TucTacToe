0364b8493f160d449b928e112d2d738734e360f4
FenrirRhogar <103751641+FenrirRhogar@users.noreply.github.com>
2022-05-26 16:57:52 +0300

    ti

--- view/HallOfFame.java
+++ view/HallOfFame.java
@@ -1,25 +1,34 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
 
 import model.Player;
+import model.PlayerRoster;
 
 public class HallOfFame {
 
-    Player player;
+    Player player = new Player();
+    PlayerRoster roster = new PlayerRoster();
 
-    public void halloffameboard() {
+    public void halloffameboard(JFrame frame) {
         JLabel title = new JLabel("Hall Of Fame");
-        title.setBounds(50, 50, 100, 30);
-        printhalloffamelist();
+        title.setBounds(278, 10, 150, 50);
+        title.setFont(new Font("Monaco", Font.BOLD, 20));
+        title.setForeground(Color.RED);
+        title.setVisible(false);
+        printhalloffamelist(frame);
     }
 
-    public void printhalloffamelist() {
-        // JLabel list = new JLabel();
+    public void printhalloffamelist(JFrame frame) {
+        JPanel list = new JPanel();
+        list.setBounds(278, 110, 150, 900);
+        frame.add(list);
         // sort players
-        for (int i = 0; i < 10; i++) {
+        /*for (i = 0; i < 10; i++) {
             // list.setText(i+1 + ". " + player.username);
 
-        }
+        }*/
     }
 }
